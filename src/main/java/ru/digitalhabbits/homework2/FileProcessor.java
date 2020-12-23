c518bfa308f8c0caf8ea2b70c9d7f7cf668e44c2
Alex <lokenaraleksandr@gmail.com>
2020-12-23 15:36:20 +0300

    Rubtsov Alex

--- src/main/java/ru/digitalhabbits/homework2/FileProcessor.java
+++ src/main/java/ru/digitalhabbits/homework2/FileProcessor.java
@@ -1,11 +1,17 @@
 package ru.digitalhabbits.homework2;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.slf4j.Logger;
 
 import javax.annotation.Nonnull;
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.concurrent.Exchanger;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static java.lang.Runtime.getRuntime;
 import static java.nio.charset.Charset.defaultCharset;
@@ -19,20 +25,65 @@ public class FileProcessor {
         checkFileExists(processingFileName);
 
         final File file = new File(processingFileName);
+        final File resultFile = new File(resultFileName);
+        if (!resultFile.exists()) {
+            try {
+                resultFile.createNewFile();
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
+        }
+        final Exchanger<Pair<String, Integer>> exchanger = new Exchanger<>();
+        final AtomicInteger rawCounter = new AtomicInteger(1);
         // TODO: NotImplemented: запускаем FileWriter в отдельном потоке
+        final Thread fileWriterThread = new Thread(new FileWriter(exchanger, resultFile, rawCounter));
+        final LineProcessor<Integer> counterProcessor = new LineCounterProcessor();
+        fileWriterThread.setDaemon(true);
+        fileWriterThread.start();
+        ExecutorService executorService = Executors.newFixedThreadPool(CHUNK_SIZE);
 
         try (final Scanner scanner = new Scanner(file, defaultCharset())) {
+            int countRaw = 1;
             while (scanner.hasNext()) {
+                int finalCountRaw = countRaw;
+                String nextLine = scanner.nextLine();
+                executorService.execute(new Runnable() {
+                    int myCount = finalCountRaw;
+                    @Override
+                    public void run() {
+                        Pair<String, Integer> process = counterProcessor.process(nextLine);
+                        while (myCount != rawCounter.get()) {
+                            try {
+                                Thread.sleep(10);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                                return;
+                            }
+                        }
+                        try {
+                            exchanger.exchange(process);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
                 // TODO: NotImplemented: вычитываем CHUNK_SIZE строк для параллельной обработки
 
                 // TODO: NotImplemented: обрабатывать строку с помощью LineProcessor. Каждый поток обрабатывает свою строку.
 
                 // TODO: NotImplemented: добавить обработанные данные в результирующий файл
+                countRaw++;
             }
         } catch (IOException exception) {
             logger.error("", exception);
         }
-
+        try {
+            executorService.awaitTermination(3, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        executorService.shutdown();
+//        fileWriterThread.interrupt();
         // TODO: NotImplemented: остановить поток writerThread
 
         logger.info("Finish main thread {}", Thread.currentThread().getName());
